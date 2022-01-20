e7a4d35b8ee4d759deb3b6c2f0d89a3bc9cf84f4
Hongling Zhang <jolinhlzhang@email.com>
2022-01-19 22:52:59 -0700

    w2d3 Pair Programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+function transpose(matrix) {
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+  
+    let grid = [];
+    for (let col = 0; col < cols; col++) {
+      grid[col] = [];
     }
-}
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols; col++) {
+        grid[col][row] = matrix[row][col]
+      }
+    }
+    return grid;
+  }
 
-module.exports = wordSearch
+
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(element => element.join(''));
+    const verticalJoin = transpose(letters).map(element => element.join(''));
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+    for (const v of verticalJoin) {
+        if (v.includes(word)) return true;
+    }
+    return false;
+};
+
+
+module.exports = wordSearch;
